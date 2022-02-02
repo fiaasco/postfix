e2f9547a3284e92101d3d2cb5d56840bbc649373
Luc Stroobant <luc@warp.be>
2022-02-02 19:12:32 +0100

    fix certificate issues

--- molecule/default/tests/test_postfix_tls.py
+++ molecule/default/tests/test_postfix_tls.py
@@ -3,7 +3,7 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('postfix-debian-10')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('postfix-debian-11')
 
 
 def test_postfix_tls_main(host):
@@ -16,18 +16,18 @@ def test_postfix_tls_main(host):
     assert f.contains('smtpd_use_tls = yes')
     assert f.contains('smtp_tls_security_level = may')
     assert f.contains('lmtp_tls_security_level = may')
-    assert f.contains('smtpd_tls_cert_file = /etc/ssl/postfix-debian-10/postfix_postfix-debian-10.chain')
-    assert f.contains('smtpd_tls_key_file = /etc/ssl/postfix-debian-10/postfix_postfix-debian-10.key')
+    assert f.contains('smtpd_tls_cert_file = /etc/ssl/postfix-debian-11/postfix_postfix-debian-11.chain')
+    assert f.contains('smtpd_tls_key_file = /etc/ssl/postfix-debian-11/postfix_postfix-debian-11.key')
 
 
 @pytest.mark.parametrize('item', ['chain', 'key'])
 def test_postfix_tls_chain(host, item):
     """ basic configuration checks for TLS
     """
-    f = host.file('/etc/ssl/postfix-debian-10/postfix_postfix-debian-10.%s' % item)
+    f = host.file('/etc/ssl/postfix-debian-11/postfix_postfix-debian-11.%s' % item)
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'postfix'
+    assert f.group == 'postfix'
 
 
 def test_postfix_tls(host):
--- molecule/rocky/tests/test_postfix_tls.py
+++ molecule/rocky/tests/test_postfix_tls.py
@@ -3,7 +3,7 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('postfix-centos')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('postfix')
 
 
 def test_postfix_tls_main(host):
@@ -16,24 +16,24 @@ def test_postfix_tls_main(host):
     assert f.contains('smtpd_use_tls = yes')
     assert f.contains('smtp_tls_security_level = may')
     assert f.contains('lmtp_tls_security_level = may')
-    assert f.contains('smtpd_tls_cert_file = /etc/ssl/postfix-centos/postfix_postfix-centos.chain')
-    assert f.contains('smtpd_tls_key_file = /etc/ssl/postfix-centos/postfix_postfix-centos.key')
+    assert f.contains('smtpd_tls_cert_file = /etc/ssl/postfix-rocky8/postfix_postfix-rocky8.chain')
+    assert f.contains('smtpd_tls_key_file = /etc/ssl/postfix-rocky8/postfix_postfix-rocky8.key')
 
 
 @pytest.mark.parametrize('item', ['chain', 'key'])
 def test_postfix_tls_chain(host, item):
     """ basic configuration checks for TLS
     """
-    f = host.file('/etc/ssl/postfix-centos/postfix_postfix-centos.%s' % item)
+    f = host.file('/etc/ssl/postfix-rocky8/postfix_postfix-rocky8.%s' % item)
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'postfix'
+    assert f.group == 'postfix'
 
 
 def test_postfix_tls(host):
     """ check whether postfix is offering TLS connections
     """
-    command = host.run("openssl s_client -showcerts -connect localhost:25 -msg -ssl3")
+    command = host.run("openssl s_client -showcerts -connect localhost:25 -msg -tls1_3")
     assert "CONNECTED" in command.stdout
-    assert "SSL 3.0 Handshake" in command.stdout
-    assert "Protocol  : SSLv3" in command.stdout
+    assert "TLS 1.3, Handshake" in command.stdout
+    assert "SSL handshake has read" in command.stdout
